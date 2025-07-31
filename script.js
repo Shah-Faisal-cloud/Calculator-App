81be958ee12a6abe3eef7353e86371063b98f5ca
Shah-Faisal-cloud <fjcreater99@gmail.com>
2025-07-31 10:28:45 +0500

    Uploading

---
+++ script.js
@@ -0,0 +1,43 @@
+const body = document.body;
+let slider = document.getElementById("theme-slider");
+let input = document.querySelector("input[type='text']");
+const buttons = document.querySelectorAll("button");
+
+let str = '';
+let lastValidChar = "";
+const themes = ["theme-one", "theme-two", "theme-three"];
+const validExpression = /^-?\d+(\.\d+)?([+\-*/]-?\d+(\.\d+)?)*$/;
+
+slider.addEventListener("input", changeTheme);
+buttons.forEach(button => {
+    button.addEventListener("click", calculate);
+});
+input.addEventListener("keydown", validateInput);
+input.addEventListener("input", validateInput);
+
+
+function changeTheme() {
+    themes.forEach(theme => body.classList.remove(theme));
+    body.classList.add(themes[+slider.value - 1]);
+};
+
+function calculate(e) {
+    if (e.target.dataset.value === 'Del') {
+        str = str.substring(0, str.length - 1);
+        input.value = str;
+    } else if (e.target.dataset.value === 'Reset') {
+        str = "";
+        input.value = str;
+    } else if (e.target.dataset.value === "=") {
+        str = input.value;
+        if (!validExpression.test(str)) {
+            input.value = "Invalid Input"
+            return;
+        };
+        str = eval(str);
+        input.value = str;
+    } else {
+        str += e.target.dataset.value;
+        input.value = str;
+    };
+};
